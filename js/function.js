42f059076eee48acbfe2ab51c7a221a6f6d024df
Shaila Nasrin <drshaila2021@gmail.com>
2022-02-17 00:19:14 -0600

    fourth- commit

--- js/function.js
+++ js/function.js
@@ -1,51 +1,45 @@
-function expenceAmount(expence){
+function moneyAmount(money){
       
-      const expenceInput =document.getElementById(expence+'-input');
-      const expenceInputText =expenceInput.value;
-      if (isNaN(expenceInputText)){
+      const moneyInput =document.getElementById(money+'-input');
+      const moneyInputText =moneyInput.value;
+      if (isNaN(moneyInputText)){
             const massageString =document.getElementById('massage-string'); 
-      const massageNumber = document.getElementById('massage-number');
+            const massageNumber = document.getElementById('massage-number');
           massageString.style.display='block';
           massageNumber.style.display='none';  
             console.log('Please enter valid numbers')
-      } 
-      else if(expenceInputText<0){
+            
+      }
+      else if(moneyInputText<0){
             const massageString =document.getElementById('massage-string'); 
       const massageNumber = document.getElementById('massage-number');
 massageNumber.style.display='block';
 massageString.style.display='none';
-      }
+      } 
+      
       else {
-            const massageString =document.getElementById('massage-string'); 
-      const massageNumber = document.getElementById('massage-number');
-            massageString.style.display='none';
-            massageNumber.style.display='none';      
-      const expenceInputNumber=parseFloat(expenceInputText);
-      return expenceInputNumber;
-      console.log(expenceInputNumber)
+      //       const massageString =document.getElementById('massage-string'); 
+      // const massageNumber = document.getElementById('massage-number');
+            // massageString.style.display='none';
+            // massageNumber.style.display='none';      
+      const moneyInputNumber=parseFloat(moneyInputText);
+       return moneyInputNumber;
+
+      console.log(moneyInputNumber)
       }
 }
 
 // Add enent handler at calculate button
 document.getElementById("calculate-btn").addEventListener('click',function(){
       // income input
-      const income =expenceAmount('income');
+      const income =moneyAmount('income');
       console.log(income)
         //  food input  
-   const foodExpence = expenceAmount('food')
+   const foodExpence = moneyAmount('food')
 // rent input
-const rentExpence = expenceAmount('rent')
+const rentExpence = moneyAmount('rent')
 // cloth input
-const clothExpence = expenceAmount('cloth')
-
-
-// if (isNaN(income || foodExpence || rentExpence ||clothExpence )){
-//       console.log('please enter number')
-
-// } else if (income <0 || foodExpence <0 || rentExpence <0 ||clothExpence <0){
-//       console.log('please enter positive number')
-
-// } else {
+const clothExpence = moneyAmount('cloth')
 
 // total expences calculate
 const totalExpence =document.getElementById('total-expences')
@@ -68,6 +62,40 @@ const balanceTotal = income-totalExpenceAmount;
 balance.innerText=balanceTotal;
 console.log(balanceTotal)
 }
-                                                                      
+
+})
+// add event handler on save button
+document.getElementById('save-btn').addEventListener('click',function(){
+      // get income 
+      const income =moneyAmount('income'); 
+      // get persentage value
+    const persentageInput= document.getElementById('persentage-input');
+    const persentageText = persentageInput.value;
+    const persentage= parseFloat(persentageText);
+//    get saving amount
+ const saveAmount= document.getElementById('save-amount');
+ let totalSaveAmount=persentage/100*income;
+ saveAmount.innerText=totalSaveAmount;
+//  get balance
+const balance = document.getElementById('balance');
+  const balanceTotal2=parseFloat( balance.innerText);
+  if(totalSaveAmount>balanceTotal2){
+  const massageSmaller = document.getElementById('massage-smaller');
+  massageSmaller.style.display='block';
+  saveAmount.innerText='';
+  
+  }
+  else{
+      const massageSmaller = document.getElementById('massage-smaller');
+      massageSmaller.style.display='none';
+      saveAmount.innerText=totalSaveAmount;
+      console.log(saveAmount.innerText)
+// console.log(totalSaveAmount);
+     const remainingBalance = document.getElementById('remaining-balance');
+     const remaining= balanceTotal2- totalSaveAmount;
+     remainingBalance.innerText=remaining;
+//      console.log(remainingBalance.innerText)
+console.log(remaining)
+  } 
 
 })
